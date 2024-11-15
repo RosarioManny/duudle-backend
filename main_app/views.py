c9247f38440452ccfaf4ccbff390f9e5f5fdf828
RosarioManny <rosario.emm47@gmail.com>
2024-11-15 16:50:33 -0500

    Import generics

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -8,4 +9,4 @@ class Home(APIView):
     content = {'message': 'welcome to Whataduudle'}
     return Response(content)
 
-class GameDetail()
+class GameDetail)
