ef995ce2980f5a3dd1cf3b720c8479808d0d1de8
RosarioManny <rosario.emm47@gmail.com>
2024-11-15 18:05:12 -0500

    User

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Word Model
 DIFFICULTY_CHOICES = (
@@ -14,6 +15,9 @@ class Word(models.Model):
 class Game(models.Model):
   winner = models.BooleanField() # <--- True
   current_word = models.ManyToManyField(Word) #<--- Later Pass in Word Model
-  user = models.OneToOneField(User) # <---- Later pass in User
+  user = models.OneToOneField(User, on_delete=models.CASCADE) # <---- Later pass in User
   created_at = models.DateTimeField(auto_now_add=True) # <--- Auto Adds a Timestapmp of When game was created. 
 
+class Drawing(models.Model):
+  art = models.JSONField()
+  game_id = models.OneToOneField
--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Game
+from .models import Game, Word
+from django.contrib.auth.models import User
+from .serializers import UserSerializer, GameSerializer
 
-# Create your views here.
+# Create your views here.g
 class Home(APIView):
   def get(self, request):
     content = {'message': 'welcome to Whataduudle'}
     return Response(content)
 
-class GameDetail(generics.RetrieveUpdateDestroyAPIView):
+class GameDetails(generics.RetrieveUpdateDestroyAPIView):
   queryset = Game.objects.all()
   fields = '__all__'
+
+class CreateUserView(generics.CreateAPIView):
+  
+class LoginView(APIView):
+  
+class VerifyUserView(APIView):
+  permission_classes = [permissions.IsAuthenticated]
+
+  def get(self, request):
+    user = User.objects.get(username=request.user)  # Fetch user profile
+    refresh = RefreshToken.for_user(request.user)  # Generate new refresh token
+    return Response ({
+      'refresh': str(refresh),
+      'access': str(refresh.access_token),
+      'user': UserSerializer(user).data
+    })
