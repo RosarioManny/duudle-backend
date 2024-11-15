f2b56ad40417fd6e36bf5d9c749bca5a99af52c4
Mandy2114 <mandydecamp1213@gmail.com>
2024-11-15 17:48:40 -0500

    Pulling

--- main_app/views.py
+++ main_app/views.py
@@ -12,3 +12,36 @@ class Home(APIView):
  def get(self, request):
   content = {'message': 'welcome to Whataduudle'}
   return Response(content)
+ 
+class CreateUserView(generics.CreateAPIView):
+ queryset = User.objects.all()
+ serializer_class = UserSerializer
+
+ def create(self, request, *arg, **kwargs):
+  response = super().create(request, *arg, **kwargs)
+  user = User.objects.get(username=response.data['username'])
+  refresh = RefreshToken.for_user(user)
+  return Response({
+   'refresh': str(refresh),
+   'access': str(refresh.access_token),
+   'user': response.data
+  })
+ 
+ class LoginView(APIView):
+  permission_classes = [permissions.AllowAny]
+
+  def post(self, request):
+   username = request.data.get('username')
+   password = request.data.get('password')
+   user = authenticate(username=username, password=password)
+   if user:
+    refresh = RefreshToken.for_user(user)
+    return Response({
+     'refresh': str(refresh),
+     'access': str(refresh.access_token),
+     'user': UserSerializer(user).data
+    })
+   return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+  
+class VerifyUserView(APIView):
+ permission_classes = [permissions.IsAuthenticated]
