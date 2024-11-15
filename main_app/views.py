61a41f78591b33e60950116cffa8f4f34d651d0e
Mandy2114 <mandydecamp1213@gmail.com>
2024-11-15 17:12:15 -0500

    Saveing

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import generics, status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework.exceptions import PermissionDenied
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from .models import Game, Drawing, Word
+from .serializers import UserSerializer
 # Create your views here.
 class Home(APIView):
  def get(self, request):
