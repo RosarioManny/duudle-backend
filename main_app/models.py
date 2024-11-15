f3edefd3adfaa14e18d372769fd687dee0d1f329
RosarioManny <rosario.emm47@gmail.com>
2024-11-15 16:22:06 -0500

    Create Game Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    winner = models.BooleanField() # <--- True
+    current_word = models.ManyToManyField() #<--- Later Pass in Word Model
+    user_id = models.OneToOneField() # <---- Later pass in User
+    created_at = models.DateTimeField(auto_now_add=True) # <--- Auto Adds a Timestapmp of When game was created. 
