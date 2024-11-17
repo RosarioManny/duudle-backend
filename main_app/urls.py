e72d1c6a80cafce653b92b886809cf4dd0a57844
RosarioManny <rosario.emm47@gmail.com>
2024-11-17 13:32:02 -0500

    Changes

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Home, CreateUserView, LoginView, VerifyUserView, GameDetails
+from .views import Home, GameDetails
+# CreateUserView, LoginView, VerifyUserView, 
 
 urlpatterns = [
   path('', Home.as_view(), name='home'),
   path('users/register/',CreateUserView.as_view(), name='register'),
   path('users/login/', LoginView.as_view(), name='login'),
   path('users/token/refresh/', VerifyUserView.as_view(), name='token_refesh'),
-  path('/games/<int:game_id>/', GameDetails.as_view(), name='Game-Details'),
+  path('/games/<int:game_id>/', GameDetails.as_view(), name='game-details'),
 ]
