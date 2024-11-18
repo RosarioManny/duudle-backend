273d4c56f844540c791a9bf13c68936b5d385872
RosarioManny <rosario.emm47@gmail.com>
2024-11-18 10:55:23 -0500

    Model migrate

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
   path('users/token/refresh/', VerifyUserView.as_view(), name='token_refesh'),
   path('games/<int:game_id>/', GameDetails.as_view(), name='Game-Details'),
   path('words/', WordList.as_view(), name='word-list'),
-  path('words/<int:word_id>/', WordDetail.as_view(), name='word-list'),
-  path('words/<int:word_id>/game', WordGame.as_view(), name='Word-Game') 
+  path('words/<int:id>/', WordDetail.as_view(), name='word-list'),
+  path('words/<int:id>/game', WordGame.as_view(), name='Word-Game') 
 ]
