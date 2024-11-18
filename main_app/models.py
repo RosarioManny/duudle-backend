8edd654df578242fa4f4f7d62eac7e918eba4074
RosarioManny <rosario.emm47@gmail.com>
2024-11-18 10:39:49 -0500

    Models Drawing - Sketch

--- main_app/models.py
+++ main_app/models.py
@@ -3,28 +3,37 @@ from django.contrib.auth.models import User
 
 # Word Model
 DIFFICULTY_CHOICES = (
-  ('easy', 'Easy'),
-  ('medium', 'Medium'),
-  ('hard', 'Hard'),
+  ('EASY', 'Easy'),
+  ('MEDIUM', 'Medium'),
+  ('HARD', 'Hard'),
 )
 
 class Word(models.Model):
   prompt = models.CharField(max_length=100)
-  difficulty = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES, default='easy')
+  difficulty = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES, default='EASY')
+
+  def __str__(self):
+    return self.prompt
 
 class Game(models.Model):
   result = models.BooleanField(default=False) # <--- False
   word = models.ManyToManyField(Word) #<--- Later Pass in Word Model
-  sketch = models.CharField() # <------- Drawing model ?
+  sketch = models.CharField() # <------- Drawing model ? Tell Front End
   user = models.OneToOneField(User, on_delete=models.CASCADE) # <---- Later pass in User
   created_at = models.DateTimeField(auto_now_add=True) # <--- Auto Adds a Timestapmp of When game was created. 
   difficulty = models.CharField(
     choices=DIFFICULTY_CHOICES, 
     default=DIFFICULTY_CHOICES[0][0]
     )
+  
+  def __str__(self): 
+    return f"{self.word} | {self.result}"
 
 class Drawing(models.Model):
   game_id = models.OneToOneField(Game, on_delete=models.CASCADE)
   # game_id = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='drawings')  # Add related_name
   art = models.JSONField()
   # prediction = models.CharField()
+
+  def __str__(self):
+    return self.game_id
