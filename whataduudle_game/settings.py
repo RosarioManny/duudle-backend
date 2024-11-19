dced3c51225a8e6beeb83c3d2833b5d1422bdfde
Timothy Lim <contacttimothylim@gmail.com>
2024-11-19 16:36:24 -0500

    user from onetoone to foreignkey and setting added cors config and moved middleware to the top

--- whataduudle_game/settings.py
+++ whataduudle_game/settings.py
@@ -29,9 +29,32 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:5173", 
+    "http://127.0.0.1:5173",  
+    "http://localhost:3000",  
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -42,6 +65,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'main_app',
     'corsheaders'
 ]
