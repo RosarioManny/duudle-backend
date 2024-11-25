01217ea558048fe4295dc34c266f4a22119ad411
RosarioManny <rosario.emm47@gmail.com>
2024-11-25 13:10:04 -0500

    CORS Adjustment

--- whataduudle_game/settings.py
+++ whataduudle_game/settings.py
@@ -38,8 +38,12 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", 
     "http://127.0.0.1:5173",  
     "http://localhost:3000",  
-    "https://whataduudle.netlify.app",
+    "https://whataduudle.netlify.app/",
     # "insert netlify url here",  # chage this to your netlify app url
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "https://whataduudle.netlify.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
